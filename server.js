f99789c6708e000c69aca63b8e3d50f988d0ca6d
Jon Peach <jonmakesitbetter@gmail.com>
2020-10-08 19:05:05 -0400

    Corrected server.js file and refactored cat.js to suit burger.js functionality

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
 
 const app = express();
 
@@ -14,10 +13,10 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "J@eger84",
-  database: "burger_db",
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
